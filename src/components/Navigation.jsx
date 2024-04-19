17bb6f78ae7a06200ec23d3f8d3e9b0c12df51ea
Aloysius J Kolapran <aloysiusjkolapran001@gmail.com>
2024-04-19 14:13:19 +0530

    initial-commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+const Navigation=() => {
+    return(
+        <nav className="container">
+            <div classname="logo"></div>
+            <img src="/Images/brand_logo.png" alt="Logo" />
+            <ul>
+            <li href="#">Menu</li>
+            <li href="#">Location</li>
+            <li href="#">About</li>
+            <li href="#">Contact</li>
+            </ul>
+            <button>Login</button>
+      </nav>
+    );
+};
+export default Navigation
